fc4c49b7b63fe6285d124ae644c83fef3b35e2b7
Saro <passarosaro@gmail.com>
2020-07-10 15:11:35 +0100

    changes

--- realworld_benchmark/data/molecules.py
+++ realworld_benchmark/data/molecules.py
@@ -144,14 +144,12 @@ class MoleculeDataset(torch.utils.data.Dataset):
         """
         start = time.time()
         print("[I] Loading dataset %s..." % (name))
-        print("I'm here")
         self.name = name
-        data_dir = 'realworld_benchmark/data/'
+        data_dir = 'data/'
         with open(data_dir + name + '.pkl', "rb") as f:
-            print("second")
             f = pickle.load(f)
-
             self.train = f[0]
+            print(f[0])
             self.val = f[1]
             self.test = f[2]
             self.num_atom_type = f[3]
