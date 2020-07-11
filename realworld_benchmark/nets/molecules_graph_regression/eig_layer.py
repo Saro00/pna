a82c5acf44227dbc908061514b1b88841d9359f0
Saro <passarosaro@gmail.com>
2020-07-11 15:06:33 +0100

    changes

--- realworld_benchmark/nets/molecules_graph_regression/eig_layer.py
+++ realworld_benchmark/nets/molecules_graph_regression/eig_layer.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .aggregators import AGGREGATORS
+from .aggregators import AGGREGATORS, aggregate_NN
 from .layers import MLP, FCLayer
 from .scalers import SCALERS
 
