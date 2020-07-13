8a3cc73a81016b73b6b0a6b526af7b8669909480
Saro <passarosaro@gmail.com>
2020-07-13 21:10:54 +0100

    changes

--- realworld_benchmark/nets/molecules_graph_regression/eig_layer.py
+++ realworld_benchmark/nets/molecules_graph_regression/eig_layer.py
@@ -68,7 +68,7 @@ class EIGLayer(nn.Module):
         if self.NN_eig:
             #w1 = self.eigfilt1(torch.cat([eig_s[:, :, 1].unsqueeze(-1), eig_d[:][:, :, 1].unsqueeze(-1)], dim=-1))
             #w2 = self.eigfilt2(torch.cat([eig_s[:, :, 2].unsqueeze(-1), eig_d[:][:, :, 2].unsqueeze(-1)], dim=-1))
-            w = self.eigfilt(torch.cat([eig_s[:, :, i].unsqueeze(-1) for in range(10)] +
+            w = self.eigfilt(torch.cat([eig_s[:, :, i].unsqueeze(-1) for i in range(10)] +
                                        [eig_d[:][:, :, i].unsqueeze(-1) for i in range(10)], dim=-1))
             #e1 = aggregate_NN(h, w1)
             #e2 = aggregate_NN(h, w2)
