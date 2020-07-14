9222f5cde86b9533e0a32c86788fd53994278e48
Saro <passarosaro@gmail.com>
2020-07-14 19:47:30 +0100

    changes

--- realworld_benchmark/main_molecules.py
+++ realworld_benchmark/main_molecules.py
@@ -183,9 +183,6 @@ def train_val_pipeline(dataset, params, net_params, dirs):
     print("TOTAL TIME TAKEN: {:.4f}s".format(time.time() - t0))
     print("AVG TIME PER EPOCH: {:.4f}s".format(np.mean(per_epoch_time)))
 
-    for _ in range(10):
-        print('Sampled value is ', model.layers[1].eigfilt(torch.Tensor([random.random() for i in range(6)])))
-   # print('Value for ', (i - 10) / 10, (j - 10) / 10, ' is ', model.layers[1].eigfilt2.forward((i - 10) / 10, (j - 10) / 10))
 
     writer.close()
 
