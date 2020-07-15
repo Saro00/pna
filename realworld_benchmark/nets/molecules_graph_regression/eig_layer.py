6ce9c053372f4ddd83f4f27f08efca2740e88d32
Saro <passarosaro@gmail.com>
2020-07-15 14:45:37 +0100

    changes

--- realworld_benchmark/nets/molecules_graph_regression/eig_layer.py
+++ realworld_benchmark/nets/molecules_graph_regression/eig_layer.py
@@ -29,13 +29,13 @@ class EIGTower(nn.Module):
         self.pretrans = MLP(in_size=2 * in_dim + (edge_dim if edge_features else 0), hidden_size=in_dim,
                             out_size=in_dim, layers=pretrans_layers, mid_activation='relu', last_activation='none')
         if NN_eig:
-            self.posttrans = MLP(in_size=((len(aggregators)+1) * len(scalers) + 1) * in_features,
-                             hidden_size=out_features,
-                             out_size=out_features, layers=posttrans_layers, mid_activation='relu', last_activation='none')
+            self.posttrans = MLP(in_size=((len(aggregators)+1) * len(scalers) + 1) * in_dim,
+                             hidden_size=out_dim,
+                             out_size=out_dim, layers=posttrans_layers, mid_activation='relu', last_activation='none')
         else:
-            self.posttrans = MLP(in_size=(len(aggregators) * len(scalers) + 1) * in_features,
-                             hidden_size=out_features,
-                             out_size=out_features, layers=posttrans_layers, mid_activation='relu', last_activation='none')
+            self.posttrans = MLP(in_size=(len(aggregators) * len(scalers) + 1) * in_dim,
+                             hidden_size=out_dim,
+                             out_size=out_dim, layers=posttrans_layers, mid_activation='relu', last_activation='none')
         self.avg_d = avg_d
         self.eigfilt = MLP(in_size=6, hidden_size=3, out_size=1, layers=3, mid_activation='relu', last_activation='none')
 
