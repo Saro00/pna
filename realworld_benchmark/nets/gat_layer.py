7e9696818cd8baf703a75c221ef40c0912405c74
Saro <passarosaro@gmail.com>
2020-07-20 10:55:45 +0100

    changes

---
+++ realworld_benchmark/nets/gat_layer.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GATLayer(nn.Module):
+    """
+    Parameters
+    ----------
+    in_dim :
+        Number of input features.
+    out_dim :
+        Number of output features.
+    num_heads : int
+        Number of heads in Multi-Head Attention.
+    dropout :
+        Required for dropout of attn and feat in GATConv
+    batch_norm :
+        boolean flag for batch_norm layer.
+    residual :
+        If True, use residual connection inside this layer. Default: ``False``.
+    activation : callable activation function/layer or None, optional.
+        If not None, applies an activation function to the updated node features.
+
+    Using dgl builtin GATConv by default:
+    https://github.com/graphdeeplearning/benchmarking-gnns/commit/206e888ecc0f8d941c54e061d5dffcc7ae2142fc
+    """
+
+    def __init__(self, in_dim, out_dim, num_heads, dropout, batch_norm, residual=False, activation=F.elu):
+        super().__init__()
+        self.residual = residual
+        self.activation = activation
+        self.batch_norm = batch_norm
+
+        if in_dim != (out_dim * num_heads):
+            self.residual = False
+
+        # equation (1)
+        self.fc = nn.Linear(in_dim, out_dim, bias=False)
+        # equation (2)
+        self.attn_fc = nn.Linear(2 * out_dim, 1, bias=False)
+
+        if self.batch_norm:
+            self.batchnorm_h = nn.BatchNorm1d(out_dim * num_heads)
+
+    def edge_attention(self, edges):
+        # edge UDF for equation (2)
+        z2 = torch.cat([edges.src['z'], edges.dst['z']], dim=1)
+        a = self.attn_fc(z2)
+        return {'e': F.leaky_relu(a)}
+
+    def message_func(self, edges):
+        # message UDF for equation (3) & (4)
+        return {'z': edges.src['z'], 'e': edges.data['e']}
+
+    def reduce_func(self, nodes):
+        # reduce UDF for equation (3) & (4)
+        # equation (3)
+        alpha = F.softmax(nodes.mailbox['e'], dim=1)
+        # equation (4)
+        h = torch.sum(alpha * nodes.mailbox['z'], dim=1)
+        return {'h': h}
+
+    def forward(self, g, h):
+        h_in = h  # for residual connection
+
+        # equation (1)
+        z = self.fc(h)
+        self.g.ndata['z'] = z
+        # equation (2)
+        self.g.apply_edges(self.edge_attention)
+        # equation (3) & (4)
+        self.g.update_all(self.message_func, self.reduce_func)
+
+        h = g.ndata['h']
+
+        if self.batch_norm:
+            h = self.batchnorm_h(h)
+
+        if self.activation:
+            h = self.activation(h)
+
+        if self.residual:
+            h = h_in + h  # residual connection
+
+        return h
