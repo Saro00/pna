c3cf7cde52cf1e98907ce2603e733c1d545b3127
Saro <passarosaro@gmail.com>
2020-07-20 11:51:12 +0100

    changes

--- realworld_benchmark/main_molecules.py
+++ realworld_benchmark/main_molecules.py
@@ -166,7 +166,7 @@ def train_val_pipeline(dataset, params, net_params, dirs):
                     print("Max_time for training elapsed {:.2f} hours, so stopping".format(params['max_time']))
                     break
 
-                for _ in range(5):
+                #for _ in range(5):
                     #print('Sampled value is ', model.layers[1].towers[0].eigfiltbis(torch.FloatTensor([random.random() for i in range(4)]).to('cuda')))
 
     except KeyboardInterrupt:
@@ -186,8 +186,8 @@ def train_val_pipeline(dataset, params, net_params, dirs):
     print("Test MAE: {:.4f}".format(test_mae))
     print("TOTAL TIME TAKEN: {:.4f}s".format(time.time() - t0))
     print("AVG TIME PER EPOCH: {:.4f}s".format(np.mean(per_epoch_time)))
-    for i, layer in enumerate(model.layers):
-        for j, tower in enumerate(layer.towers):
+    #for i, layer in enumerate(model.layers):
+        #for j, tower in enumerate(layer.towers):
             #print('For layer ', i, ' tower ', j, ' the weights are ', tower.bias)
             #print('For layer ', i, ' tower ', j, ' the bias are ', tower.bias)
 
