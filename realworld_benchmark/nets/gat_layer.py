4a6506f5d4db2d360ab53d8e379be929756f15ae
Saro <passarosaro@gmail.com>
2020-07-20 11:59:55 +0100

    changes

--- realworld_benchmark/nets/gat_layer.py
+++ realworld_benchmark/nets/gat_layer.py
@@ -2,6 +2,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from dgl.nn.pytorch import GATConv
+
+"""
+    GAT: Graph Attention Network
+    Graph Attention Networks (Veličković et al., ICLR 2018)
+    https://arxiv.org/abs/1710.10903
+"""
+
 
 class GATLayer(nn.Module):
     """
@@ -35,52 +43,23 @@ class GATLayer(nn.Module):
         if in_dim != (out_dim * num_heads):
             self.residual = False
 
-        # equation (1)
-        self.fc = nn.Linear(in_dim, out_dim, bias=False)
-        # equation (2)
-        self.attn_fc = nn.Linear(2 * out_dim, 1, bias=False)
+        self.gatconv = GATConv(in_dim, out_dim, num_heads, dropout, dropout)
 
         if self.batch_norm:
             self.batchnorm_h = nn.BatchNorm1d(out_dim * num_heads)
 
-    def edge_attention(self, edges):
-        # edge UDF for equation (2)
-        z2 = torch.cat([edges.src['z'], edges.dst['z']], dim=1)
-        a = self.attn_fc(z2)
-        return {'e': F.leaky_relu(a)}
-
-    def message_func(self, edges):
-        # message UDF for equation (3) & (4)
-        return {'z': edges.src['z'], 'e': edges.data['e']}
-
-    def reduce_func(self, nodes):
-        # reduce UDF for equation (3) & (4)
-        # equation (3)
-        alpha = F.softmax(nodes.mailbox['e'], dim=1)
-        # equation (4)
-        h = torch.sum(alpha * nodes.mailbox['z'], dim=1)
-        return {'h': h}
-
     def forward(self, g, h):
         h_in = h  # for residual connection
 
-        # equation (1)
-        z = self.fc(h)
-        g.ndata['z'] = z
-        # equation (2)
-        g.apply_edges(self.edge_attention)
-        # equation (3) & (4)
-        g.update_all(self.message_func, self.reduce_func)
+        h = self.gatconv(g, h).flatten(1)
 
-        h = g.ndata['h']
-
-        if False:#self.batch_norm:
+        if self.batch_norm:
             h = self.batchnorm_h(h)
 
         if self.activation:
             h = self.activation(h)
 
-        if False:#self.residual:
+        if self.residual:
             h = h_in + h  # residual connection
 
         return h
