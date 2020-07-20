78f40a14eba0232d3872d88f2273b040764869bc
Saro <passarosaro@gmail.com>
2020-07-20 16:47:58 +0100

    changes

--- realworld_benchmark/nets/GATConvEIG.py
+++ realworld_benchmark/nets/GATConvEIG.py
@@ -55,7 +55,7 @@ class GATConvEIG(nn.Module):
         self._num_heads = num_heads
         self._in_feats = in_feats
         self._out_feats = out_feats
-        self.fc = nn.Linear(in_feats + 1, out_feats * num_heads, bias=False)
+        self.fc = nn.Linear(in_feats, out_feats * num_heads, bias=False)
         self.attn_l = nn.Parameter(th.FloatTensor(size=(1, num_heads, out_feats + 2)))
         self.attn_r = nn.Parameter(th.FloatTensor(size=(1, num_heads, out_feats + 2)))
         self.feat_drop = nn.Dropout(feat_drop)
