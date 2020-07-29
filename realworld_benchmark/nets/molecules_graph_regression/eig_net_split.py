b7dc6216683b26fa96ebbbc559a0f4aa41484874
Saro <passarosaro@gmail.com>
2020-07-29 15:20:30 +0100

    debugging model that concatenate aggr. only at the end of convolutions

--- realworld_benchmark/nets/molecules_graph_regression/eig_net_split.py
+++ realworld_benchmark/nets/molecules_graph_regression/eig_net_split.py
@@ -101,6 +101,7 @@ class EIGNetSplit(nn.Module):
 
         self.aggregators = net_params['aggregators']
         self.out_dim = net_params['out_dim']
+        print(self.aggregators)
         print(self.aggregators.split())
 
         net_params_list = []
