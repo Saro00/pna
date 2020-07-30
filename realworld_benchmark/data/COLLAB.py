9d2f18eda321792c35c77b8dad0a128321c2f81e
Saro <passarosaro@gmail.com>
2020-07-30 14:02:45 +0100

    Add COLLAB dataset for eig-model

--- realworld_benchmark/data/COLLAB.py
+++ realworld_benchmark/data/COLLAB.py
@@ -48,8 +48,9 @@ class COLLABDataset(Dataset):
             [self.graph.edata['edge_weight'], self.graph.edata['edge_year']],
             dim=1
         )
-        A = self.graph.adjacency_matrix().to_dense()
-        self.graph.ndata['eig'] = get_k_lowest_eig(A, 4)
+        #A = self.graph.adjacency_matrix().to_dense()
+        #self.graph.ndata['eig'] = get_k_lowest_eig(A, 4)
+        self.graph.ndata['eif'] = torch.rand((235868, 5))
 
         self.split_edge = self.dataset.get_edge_split()
         self.train_edges = self.split_edge['train']['edge']  # positive train edges
