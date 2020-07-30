35cb7209973496772fb2efd42274183770be98a0
Saro <passarosaro@gmail.com>
2020-07-30 15:30:25 +0100

    Add COLLAB dataset for eig-model

--- realworld_benchmark/data/COLLAB.py
+++ realworld_benchmark/data/COLLAB.py
@@ -56,7 +56,6 @@ class COLLABDataset(Dataset):
         self.val_edges_neg = self.split_edge['valid']['edge_neg']  # negative val edges
         self.test_edges = self.split_edge['test']['edge']  # positive test edges
         self.test_edges_neg = self.split_edge['test']['edge_neg']  # negative test edges
-        self.
 
         self.evaluator = Evaluator(name='ogbl-collab')
 
