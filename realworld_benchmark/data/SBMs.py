2ed34228257c9c1f5472cd105845094a4454e62b
Saro00 <54949520+Saro00@users.noreply.github.com>
2020-07-31 19:23:18 +0100

    Update SBMs.py

--- realworld_benchmark/data/SBMs.py
+++ realworld_benchmark/data/SBMs.py
@@ -139,7 +139,7 @@ def positional_encoding(g, pos_enc_dim):
     #EigVal, EigVec = sp.linalg.eigs(L, k=pos_enc_dim+1, which='SR')
     EigVal, EigVec = sp.linalg.eigs(L, k=pos_enc_dim+1, which='SR', tol=1e-2) # for 40 PEs
     EigVec = EigVec[:, EigVal.argsort()] # increasing order
-    g.ndata['eig'] = torch.from_numpy(EigVec[:,1:pos_enc_dim+1]).float64() 
+    g.ndata['eig'] = torch.from_numpy(np.real(EigVec[:,1:pos_enc_dim+1])).float() 
 
     return g
 
