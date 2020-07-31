e9cc4866655a59e0ef095a178f66ab4cf7e1273a
Saro00 <54949520+Saro00@users.noreply.github.com>
2020-07-31 19:06:46 +0100

    Update main_SBMs_node_classification.py

--- realworld_benchmark/main_SBMs_node_classification.py
+++ realworld_benchmark/main_SBMs_node_classification.py
@@ -297,6 +297,7 @@ def main():
     parser.add_argument('--posttrans_layers', type=int, help='posttrans_layers.')
     
     args = parser.parse_args()
+    print(args.config)
     with open(args.config) as f:
         config = json.load(f)
         
