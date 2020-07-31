edcd5e424561376c0c29dc365723a4686d5e655e
Saro00 <54949520+Saro00@users.noreply.github.com>
2020-07-31 19:17:47 +0100

    Update SBMs.py

--- realworld_benchmark/data/SBMs.py
+++ realworld_benchmark/data/SBMs.py
@@ -154,7 +154,7 @@ class SBMsDataset(torch.utils.data.Dataset):
         start = time.time()
         print("[I] Loading dataset %s..." % (name))
         self.name = name
-        data_dir = 'data/SBMs/'
+        data_dir = 'data/'
         with open(data_dir+name+'.pkl',"rb") as f:
             f = pickle.load(f)
             self.train = f[0]
