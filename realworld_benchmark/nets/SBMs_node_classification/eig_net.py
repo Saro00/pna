7dfa67e0531fa5af927ce693557a72c01cf34233
Saro <passarosaro@gmail.com>
2020-08-02 14:58:25 +0100

    debugging

--- realworld_benchmark/nets/SBMs_node_classification/eig_net.py
+++ realworld_benchmark/nets/SBMs_node_classification/eig_net.py
@@ -64,7 +64,8 @@ class EIGNet(nn.Module):
         if self.gru_enable:
             self.gru = GRU(hidden_dim, hidden_dim, device)
             
-        self.MLP_layer = MLPReadout(out_dim, n_classes)    
+        self.MLP_layer = MLPReadout(out_dim, n_classes)
+        self.reset_params()
         
         
        
