9900babeca52525b8661ed0a04cbe3b17b09eab5
Saro <passarosaro@gmail.com>
2020-08-13 16:50:54 +0100

    Fix bug

--- realworld_benchmark/train/train_HIV_graph_classification.py
+++ realworld_benchmark/train/train_HIV_graph_classification.py
@@ -19,7 +19,8 @@ def train_epoch_sparse(model, optimizer, device, data_loader, epoch):
     model.train()
     epoch_loss = 0
     epoch_train_ROC = 0
-
+    list_scores = []
+    list_labels = []
     for iter, (batch_graphs, batch_labels) in enumerate(data_loader):
         batch_x = batch_graphs.ndata['feat'].to(device)  # num x feat
         batch_e = batch_graphs.edata['feat'].to(device)
@@ -30,10 +31,13 @@ def train_epoch_sparse(model, optimizer, device, data_loader, epoch):
         loss.backward()
         optimizer.step()
         epoch_loss += loss.detach().item()
-        evaluator = Evaluator(name='ogbg-molhiv')
-        epoch_train_ROC += evaluator.eval({'y_pred': torch.sigmoid(batch_scores), 'y_true': batch_labels.type(torch.FloatTensor).to('cuda').unsqueeze(-1)})['rocauc']
+        list_scores.append(torch.sigmoid(batch_scores))
+        list_labels.append(batch_labels.unsqueeze(-1))
+
     epoch_loss /= (iter + 1)
-    epoch_train_ROC /= (iter + 1)
+    evaluator = Evaluator(name='ogbg-molhiv')
+    epoch_train_ROC += evaluator.eval({'y_pred': torch.cat(list_scores),
+                                       'y_true': torch.cat(list_labels)})['rocauc']
 
     return epoch_loss, epoch_train_ROC, optimizer
 
@@ -43,6 +47,8 @@ def evaluate_network_sparse(model, device, data_loader, epoch):
     epoch_test_loss = 0
     epoch_test_ROC = 0
     with torch.no_grad():
+        list_scores = []
+        list_labels = []
         for iter, (batch_graphs, batch_labels) in enumerate(data_loader):
             batch_x = batch_graphs.ndata['feat'].to(device)
             batch_e = batch_graphs.edata['feat'].to(device)
@@ -50,9 +56,12 @@ def evaluate_network_sparse(model, device, data_loader, epoch):
             batch_scores = model.forward(batch_graphs, batch_x, batch_e, True, True)
             loss = model.loss(batch_scores, batch_labels)
             epoch_test_loss += loss.detach().item()
-            evaluator = Evaluator(name='ogbg-molhiv')
-            epoch_test_ROC += evaluator.eval({'y_pred': torch.sigmoid(batch_scores), 'y_true': batch_labels.unsqueeze(-1)})['rocauc']
+            list_scores.append(torch.sigmoid(batch_scores))
+            list_labels.append(batch_labels.unsqueeze(-1))
+
         epoch_test_loss /= (iter + 1)
-        epoch_test_ROC /= (iter + 1)
+        evaluator = Evaluator(name='ogbg-molhiv')
+        epoch_train_ROC += evaluator.eval({'y_pred': torch.cat(list_scores),
+                                           'y_true': torch.cat(list_labels)})['rocauc']
 
     return epoch_test_loss, epoch_test_ROC
