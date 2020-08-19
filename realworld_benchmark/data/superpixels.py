7f513204446460fd2759f7ded6d9b07a1664aeb9
Saro <passarosaro@gmail.com>
2020-08-19 13:44:47 +0100

    Add aggr. with coordinates

--- realworld_benchmark/data/superpixels.py
+++ realworld_benchmark/data/superpixels.py
@@ -560,8 +560,8 @@ def sort_list_eig(list):
     return list_new
 
 def coord_encoding(graph):
-    x = graph.ndata['feat'][:, 3].type(torch.FloatTensor)
-    y = graph.ndata['feat'][:, 4].type(torch.FloatTensor)
+    x = graph.ndata['feat'][:, 3:4].type(torch.FloatTensor)
+    y = graph.ndata['feat'][:, 4:5].type(torch.FloatTensor)
     null = torch.zeros(x.shape).type(torch.FloatTensor)
     print(x.shape)
     print(y.shape)
