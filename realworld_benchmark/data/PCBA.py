3e09ae55b51dd4d210458c904bb0e506fe1588e9
Saro <passarosaro@gmail.com>
2020-08-21 16:55:17 +0100

    Add download script (mod.)

--- realworld_benchmark/data/PCBA.py
+++ realworld_benchmark/data/PCBA.py
@@ -3,7 +3,6 @@ import dgl
 import torch
 from torch.utils.data import Dataset
 import random as rd
-from ogb.graphproppred import DglGraphPropPredDataset
 from ogb.graphproppred import Evaluator
 
 
@@ -11,6 +10,12 @@ from scipy import sparse as sp
 import numpy as np
 import itertools
 import torch.utils.data
+import pandas as pd
+import shutil, os
+import os.path as osp
+from dgl.data.utils import load_graphs, save_graphs, Subset
+from ogb.utils.url import decide_download, download_url, extract_zip
+from ogb.io.read_graph_dgl import read_csv_graph_dgl
 
 
 
@@ -37,6 +42,120 @@ def positional_encoding(g, pos_enc_dim):
     g.ndata['eig'] = torch.from_numpy(np.real(EigVec[:, :pos_enc_dim])).float()
 
     return g
+
+
+
+class DownloadPCBA(object):
+    """ Modified version of DglGraphPropPredDataset of ogb.graphproppred, that doesn't save the dataset """
+
+    def __init__(self, name='ogbg-pcba', root="data"):
+        self.name = name  ## original name, e.g., ogbg-mol-tox21
+        self.dir_name = 'ogbg_molpcba_dgl'
+        self.original_root = root
+        self.root = osp.join(root, self.dir_name)
+
+        # check version
+        # First check whether the dataset has been already downloaded or not.
+        # If so, check whether the dataset version is the newest or not.
+        # If the dataset is not the newest version, notify this to the user.
+
+        self.download_name = 'pcba'  ## name of downloaded file, e.g., tox21
+
+        self.num_tasks = 128
+        self.eval_metric = 'ap'
+        self.task_type = 'binary classification'
+        self.num_classes = 2
+
+        self.pre_process()
+
+    def pre_process(self):
+        processed_dir = osp.join(self.root, 'processed')
+        raw_dir = osp.join(self.root, 'raw')
+        pre_processed_file_path = osp.join(processed_dir, 'dgl_data_processed')
+
+        ### download
+        url = 'https://snap.stanford.edu/ogb/data/graphproppred/csv_mol_download/pcba.zip'
+        if decide_download(url):
+            path = download_url(url, self.original_root)
+            extract_zip(path, self.original_root)
+            os.unlink(path)
+            # delete folder if there exists
+            try:
+                shutil.rmtree(self.root)
+            except:
+                pass
+            shutil.move(osp.join(self.original_root, self.download_name), self.root)
+        else:
+            print("Stop download.")
+            exit(-1)
+
+        ### preprocess
+        add_inverse_edge = True
+        additional_node_files = []
+        additional_edge_files = []
+
+        graphs = read_csv_graph_dgl(raw_dir, add_inverse_edge=add_inverse_edge,
+                                    additional_node_files=additional_node_files,
+                                    additional_edge_files=additional_edge_files)
+
+        labels = pd.read_csv(osp.join(raw_dir, "graph-label.csv.gz"), compression="gzip", header=None).values
+
+        has_nan = np.isnan(labels).any()
+
+        if "classification" in self.task_type:
+            if has_nan:
+                labels = torch.from_numpy(labels).to(torch.float32)
+            else:
+                labels = torch.from_numpy(labels).to(torch.long)
+        else:
+            labels = torch.from_numpy(labels).to(torch.float32)
+
+        print('Not Saving...')
+        # save_graphs(pre_processed_file_path, graphs, labels={'labels': labels})
+
+        ### load preprocessed files
+        self.graphs = graphs
+        self.labels = labels
+
+    def get_idx_split(self, split_type=None):
+        if split_type is None:
+            split_type = self.meta_info[self.name]["split"]
+
+        path = osp.join(self.root, "split", split_type)
+
+        train_idx = pd.read_csv(osp.join(path, "train.csv.gz"), compression="gzip", header=None).values.T[0]
+        valid_idx = pd.read_csv(osp.join(path, "valid.csv.gz"), compression="gzip", header=None).values.T[0]
+        test_idx = pd.read_csv(osp.join(path, "test.csv.gz"), compression="gzip", header=None).values.T[0]
+
+        return {"train": torch.tensor(train_idx, dtype=torch.long), "valid": torch.tensor(valid_idx, dtype=torch.long),
+                "test": torch.tensor(test_idx, dtype=torch.long)}
+
+    def __getitem__(self, idx):
+        """Get datapoint with index"""
+
+        if isinstance(idx, int):
+            return self.graphs[idx], self.labels[idx]
+        elif torch.is_tensor(idx) and idx.dtype == torch.long:
+            if idx.dim() == 0:
+                return self.graphs[idx], self.labels[idx]
+            elif idx.dim() == 1:
+                return Subset(self, idx.cpu())
+
+        raise IndexError(
+            'Only integers and long are valid '
+            'indices (got {}).'.format(type(idx).__name__))
+
+    def __len__(self):
+        """Length of the dataset
+        Returns
+        -------
+        int
+            Length of Dataset
+        """
+        return len(self.graphs)
+
+    def __repr__(self):  # pragma: no cover
+        return '{}({})'.format(self.__class__.__name__, len(self))
 
 
 class PCBADGL(torch.utils.data.Dataset):
@@ -82,7 +201,7 @@ class PCBADataset(Dataset):
         if verbose:
             print("[I] Loading dataset %s..." % (name))
         self.name = name
-        self.dataset = DglGraphPropPredDataset(name = 'ogbg-molpcba')
+        self.dataset = DownloadPCBA(name = 'ogbg-molpcba')
         self.split_idx = self.dataset.get_idx_split()
         if True:
             split = {'test': torch.tensor([i for i in range(1, 300)]),
