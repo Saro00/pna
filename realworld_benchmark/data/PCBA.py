2c6743a8900b33e74103b4f943bfcdb9d2cd33a9
Saro <passarosaro@gmail.com>
2020-08-25 13:08:13 +0100

    Medium-big dataset

--- realworld_benchmark/data/PCBA.py
+++ realworld_benchmark/data/PCBA.py
@@ -171,7 +171,7 @@ class PCBADGL(torch.utils.data.Dataset):
         self.data = [g for g in data[self.split]]
         self.graph_lists = []
         self.graph_labels = []
-        for i, g in enumerate(self.data):
+        for i, g in enumerate(self.data) and rd.random() < 0.7:
             if g[0].number_of_nodes() > 5: #and rd.random() < 0.2:
                 self.graph_lists.append(g[0])
                 self.graph_labels.append(g[1])
