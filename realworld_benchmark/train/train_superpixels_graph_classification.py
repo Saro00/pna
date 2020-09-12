67a1e703a184dbfcf26a46b8dd0a7217a5aec57b
Saro <passarosaro@gmail.com>
2020-09-12 15:43:21 +0100

    Fix clone tensor problem

--- realworld_benchmark/train/train_superpixels_graph_classification.py
+++ realworld_benchmark/train/train_superpixels_graph_classification.py
@@ -26,7 +26,7 @@ def train_epoch(model, optimizer, device, data_loader, epoch, augmentation):
         batch_labels = batch_labels.to(device)
         batch_snorm_n = batch_snorm_n.to(device) # num x 1
         if augmentation:
-            batch_graphs_aug = batch_graphs.()
+            batch_graphs_aug = batch_graphs.clone()
             angle = (torch.random(batch_graphs_aug['eig'][:, 1].shape) - 0.5) / 4
             batch_graphs_aug['eig'][:, 1], batch_graphs_aug['eig'][:, 2] = (1 - angle)**(0.5) * batch_graphs_aug['eig'][:, 1] \
                                                                            + angle * batch_graphs_aug['eig'][:, 2], \
