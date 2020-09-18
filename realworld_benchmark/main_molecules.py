fdc4e6cc277e65c8b4e8a22dae5699c17d34af5e
Saro <passarosaro@gmail.com>
2020-09-18 21:25:24 +0100

    Add flip

--- realworld_benchmark/main_molecules.py
+++ realworld_benchmark/main_molecules.py
@@ -130,7 +130,7 @@ def train_val_pipeline(dataset, params, net_params, dirs):
                 start = time.time()
 
                 epoch_train_loss, epoch_train_mae, optimizer = train_epoch(model, optimizer, device, train_loader,
-                                                                           epoch, True)
+                                                                           epoch, net_params['flip'])
                 epoch_val_loss, epoch_val_mae = evaluate_network(model, device, val_loader, epoch)
 
                 epoch_train_losses.append(epoch_train_loss)
@@ -247,6 +247,8 @@ def main():
     parser.add_argument('--expid', help='Experiment id.')
     parser.add_argument('--type_net', default='simple', help='Type of net')
     parser.add_argument('--lap_norm', default='none', help='Laplacian normalisation')
+    parser.add_argument('--flip', action='store_true', default=False, help='Flip eig each epoch')
+
 
     # eig params
     parser.add_argument('--aggregators', type=str, help='Aggregators to use.')
@@ -387,6 +389,8 @@ def main():
         net_params['pos_enc_dim'] = args.pos_enc_dim
     if args.lap_norm is not None:
         net_params['lap_norm'] = args.lap_norm
+    if args.flip is not None:
+        net_params['flip'] = args.flip
 
     # ZINC
     net_params['num_atom_type'] = dataset.num_atom_type
