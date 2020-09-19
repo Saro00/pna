8fe6adb640f7c1f0afdc149c3a044b62be394071
Saro <passarosaro@gmail.com>
2020-09-19 17:19:35 +0100

    Multiplicity of eigs

--- realworld_benchmark/data/multiplicity_eig.py
+++ realworld_benchmark/data/multiplicity_eig.py
@@ -5,7 +5,7 @@ from COLLAB import COLLABDataset
 from scipy import sparse as sp
 
 
-def get_eig_val(g, pos_enc_dim=7, norm='none'):
+def get_eig_val(g, pos_enc_dim=7, norm='none', tol=1e-3):
     # Laplacian
     A = g.adjacency_matrix_scipy(return_edge_ids=False).astype(float)
     if norm == 'none':
@@ -17,11 +17,11 @@ def get_eig_val(g, pos_enc_dim=7, norm='none'):
     elif norm == 'walk':
         N = sp.diags(dgl.backend.asnumpy(g.in_degrees()).clip(1) ** -1., dtype=float)
         L = sp.eye(g.number_of_nodes()) - N * A
-    EigVal, EigVec = sp.linalg.eigs(L, k=pos_enc_dim + 1, which='SR', tol=2e-1)
+    EigVal, EigVec = sp.linalg.eigs(L, k=pos_enc_dim + 1, which='SR', tol=tol)
     return EigVal
 
 
-def get_multiplicity(DATASET_NAME, tol, dim, norm):
+def get_multiplicity(DATASET_NAME, tol, dim, norm, tol_scipy):
     if DATASET_NAME == 'ZINC':
         dataset = MoleculeDataset(DATASET_NAME)
     elif DATASET_NAME == 'SBM_PATTERN':
@@ -37,9 +37,9 @@ def get_multiplicity(DATASET_NAME, tol, dim, norm):
         train_graphs = dataset.train.graph_lists
         val_graphs = dataset.val.graph_lists
         test_graphs = dataset.test.graph_lists
-        train_eigs = [get_eig_val(g, pos_enc_dim=dim, norm=norm) for g in train_graphs]
-        val_eigs = [get_eig_val(g, pos_enc_dim=dim, norm=norm) for g in val_graphs]
-        test_eigs = [get_eig_val(g, pos_enc_dim=dim, norm=norm) for g in test_graphs]
+        train_eigs = [get_eig_val(g, pos_enc_dim=dim, norm=norm, tol=tol_scipy) for g in train_graphs]
+        val_eigs = [get_eig_val(g, pos_enc_dim=dim, norm=norm, tol=tol_scipy) for g in val_graphs]
+        test_eigs = [get_eig_val(g, pos_enc_dim=dim, norm=norm, tol=tol_scipy) for g in test_graphs]
         eigs = train_eigs + val_eigs + test_eigs
         i = 0
         n = len(eigs)
@@ -49,14 +49,19 @@ def get_multiplicity(DATASET_NAME, tol, dim, norm):
         return i / n
 
 
+
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--DATASET_NAME', help='Name of the Dataset')
     parser.add_argument('--tol', default=1e-3, help='Tolerance for multiplicity')
+    parser.add_argument('--tol_scipy', default=1e-3, help='Tolerance of scipy to compute eigs')
     parser.add_argument('--lap_norm', default='none', help='Normalisation for the Laplacian matrix')
     parser.add_argument('--dim', help='Number of eigs to compute')
     args = parser.parse_args()
+    multiplicity_prop = get_multiplicity(args.DATASET_NAME, args.tol, args.dim, args.lap_norm, args.tol_scipy)
+    print(multiplicity_prop)
 
-    print(get_multiplicity(args.DATASET_NAME, args.tol, args.dim, args.lap_norm))
+    return multiplicity_prop
 
+main()
