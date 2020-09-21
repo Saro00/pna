4a75457ad7e0f76ff4c92e520be42cfef83bd555
Saro <passarosaro@gmail.com>
2020-09-21 20:19:20 +0100

    Add directional readout

--- realworld_benchmark/nets/molecules_graph_regression/eig_net.py
+++ realworld_benchmark/nets/molecules_graph_regression/eig_net.py
@@ -104,7 +104,7 @@ class EIGNet(nn.Module):
             print(g.ndata['eig'][:, 1:2].to(self.device).shape)
             print(g.ndata['h'].to(self.device).shape)
             print( (g.ndata['eig'][:, 1:2].to(self.device) / torch.sum(torch.abs(g.ndata['eig'][:, 1:2].to(self.device)), dim=1, keepdim=True)).shape)
-            hg = torch.abs(torch.sum(g.ndata['h'].to(self.device) * g.ndata['eig'][:, 1:2].to(self.device) / torch.sum(torch.abs(g.ndata['eig'][:, 1:2].to(self.device), dim=1, keepdim=True)), keepdim=False, dim=1))
+            hg = torch.abs(torch.sum(g.ndata['h'].to(self.device) * g.ndata['eig'][:, 1:2].to(self.device) / torch.sum(torch.abs(g.ndata['eig'][:, 1:2].to(self.device)), dim=1, keepdim=True), keepdim=False, dim=1))
         else:
             hg = dgl.mean_nodes(g, 'h')  # default readout is mean nodes
 
