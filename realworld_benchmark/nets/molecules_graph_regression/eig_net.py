c1f2e9fb96030e1c10749c495ede7e0b78851318
Saro <passarosaro@gmail.com>
2020-09-21 20:12:42 +0100

    Add directional readout

--- realworld_benchmark/nets/molecules_graph_regression/eig_net.py
+++ realworld_benchmark/nets/molecules_graph_regression/eig_net.py
@@ -103,7 +103,8 @@ class EIGNet(nn.Module):
             print(torch.sum(torch.abs(g.ndata['eig'][:, 1:2].to(self.device)), dim=1).shape)
             print(g.ndata['eig'][:, 1:2].to(self.device).shape)
             print(g.ndata['h'].to(self.device).shape)
-            hg = torch.abs(torch.sum(torch.matmul(g.ndata['h'].to(self.device), g.ndata['eig'][:, 1:2].to(self.device) / torch.sum(torch.abs(g.ndata['eig'][:, 1:2].to(self.device)), dim=1)), keepdim=False, dim=1))
+            print( (g.ndata['eig'][:, 1:2].to(self.device) / torch.sum(torch.abs(g.ndata['eig'][:, 1:2].to(self.device)), dim=1, keepdim=True)).shape)
+            hg = torch.abs(torch.sum(torch.matmul(g.ndata['h'].to(self.device), g.ndata['eig'][:, 1:2].to(self.device) / torch.sum(torch.abs(g.ndata['eig'][:, 1:2].to(self.device)), dim=1, keepdim=True)), keepdim=False, dim=1))
         else:
             hg = dgl.mean_nodes(g, 'h')  # default readout is mean nodes
 
