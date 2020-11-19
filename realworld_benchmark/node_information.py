4acba26756eb606c408b8759cab58e2f2000cf41
FedericoStazi <stazi.federico@gmail.com>
2020-11-19 21:57:14 +0000

    Add features/labels using flags

--- realworld_benchmark/node_information.py
+++ realworld_benchmark/node_information.py
@@ -1,8 +1,8 @@
 import networkx
-
+import random
 
 def get_nodes_degree(graph):
-    return graph.in_degrees()
+    return list(graph.in_degrees())
 
 def get_nodes_closeness_centrality(graph):
     return list(networkx.closeness_centrality(graph.to_networkx()).values())
@@ -13,5 +13,15 @@ def get_nodes_betweenness_centrality(graph):
 def get_nodes_pagerank(graph):
     return list(networkx.algorithms.link_analysis.pagerank_alg.pagerank(graph.to_networkx()).values())
 
+def get_nodes_triangles(graph):
+    return list(networkx.algorithms.cluster.triangles(graph.to_networkx()).values())
+
+def get_nodes_random(graph):
+    return list([random.random() for _ in graph.nodes()])
+
+def get_nodes_eigenvector(graph, k=1):
+    pass
+
 NODE_INFORMATION = {'degree' : get_nodes_degree, 'closeness_centrality' : get_nodes_closeness_centrality,
-                    'betweenness_centrality' : get_nodes_betweenness_centrality, 'pagerank' : get_nodes_pagerank}
+                    'betweenness_centrality' : get_nodes_betweenness_centrality, 'pagerank' : get_nodes_pagerank,
+                    'triangles' : get_nodes_triangles}
