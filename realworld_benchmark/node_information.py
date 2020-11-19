3da78007871266641bfd3c3d30797f9148c8a192
FedericoStazi <stazi.federico@gmail.com>
2020-11-19 18:02:17 +0000

    Multiple Feature node regression

--- realworld_benchmark/node_information.py
+++ realworld_benchmark/node_information.py
@@ -4,7 +4,7 @@ def get_nodes_degree(graph):
     return graph.in_degrees()
 
 def get_nodes_closeness_centrality(graph):
-    return networkx.closeness_centrality(graph.to_networkx())
+    return networkx.closeness_centrality(graph.to_networkx()).values()
 
 def get_nodes_betweenness_centrality(graph):
-    return networkx.betweenness_centrality(graph.to_networkx())
+    return networkx.betweenness_centrality(graph.to_networkx()).values()
