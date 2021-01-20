52f434f57732c7f7827c1448c1506a8b78e105b8
Saro <passarosaro@gmail.com>
2021-01-20 23:21:12 +0000

    Add disconnected graphs problem

--- realworld_benchmark/data/PCBA.py
+++ realworld_benchmark/data/PCBA.py
@@ -48,8 +48,8 @@ def positional_encoding(g, pos_enc_dim, norm):
     EigVal, EigVec = sp.linalg.eigs(L, k=pos_enc_dim + 1, which='SR', tol=1e-4)
     EigVec = EigVec[:, EigVal.argsort()]  # increasing order
     g.ndata['eig'] = torch.from_numpy(np.real(EigVec[:, :pos_enc_dim + 1])).float()
-    print(int(g.ndata['eig'][:, 0].unique().size[0]))
-    if int(g.ndata['eig'][:, 0].unique().size[0]) != 1:
+    print(int(g.ndata['eig'][:, 0].unique().size()[0]))
+    if int(g.ndata['eig'][:, 0].unique().size()[0]) != 1:
         return positional_encoding_bis(g, pos_enc_dim, norm)
     return g
 
